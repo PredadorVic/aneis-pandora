23edf936446c0ae1f9e656d0d26db7ac3ac04010
Victor Gonzalez <victorgonzalez2005@gmail.com>
2026-08-29 22:54:45 -0300

    Adicionar e atualizar app de livros

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "aneis-v2.6.0-2026-08-15";
+const VERSION = "aneis-v2.6.0-2026-08-15-livros-integrados";
 const CACHE_SHELL = `${VERSION}-shell`;
 const CACHE_DADOS = `${VERSION}-dados`;
 const ARQUIVOS_SHELL = [
